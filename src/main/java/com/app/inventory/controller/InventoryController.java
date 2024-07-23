c9100134ae5ad098732a338f98730579ae70a7cf
wiratama.putra <wiratama.putra@mandalafinance.com>
2024-07-23 14:29:46 +0700

    fix : fix unused annotation

--- src/main/java/com/app/inventory/controller/InventoryController.java
+++ src/main/java/com/app/inventory/controller/InventoryController.java
@@ -4,7 +4,6 @@ import com.app.inventory.dto.OperationResponse;
 import com.app.inventory.entity.Product;
 import com.app.inventory.service.InventoryService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,31 +15,26 @@ public class InventoryController {
     private InventoryService inventoryService;
 
     @PostMapping("/add")
-    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     public Product addProduct(@RequestBody Product product) {
         return inventoryService.addProduct(product);
     }
 
     @PutMapping("/update/{productId}")
-    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     public OperationResponse updateProductQuantity(@PathVariable Long productId, @RequestParam Integer quantity) {
         return inventoryService.updateProductQuantity(productId, quantity);
     }
 
     @GetMapping("/check/{productId}")
-    @PreAuthorize("hasAuthority('ROLE_USER') or hasAuthority('ROLE_ADMIN')")
     public OperationResponse checkProductAvailability(@PathVariable Long productId) {
         return inventoryService.checkProductAvailability(productId);
     }
 
     @PostMapping("/order/{productId}")
-    @PreAuthorize("hasAuthority('ROLE_USER') or hasAuthority('ROLE_ADMIN')")
     public OperationResponse orderProduct(@PathVariable Long productId, @RequestParam Integer quantity) {
         return inventoryService.orderProduct(productId, quantity);
     }
 
     @GetMapping("/products")
-    @PreAuthorize("hasAuthority('ROLE_USER') or hasAuthority('ROLE_ADMIN')")
     public List<Product> getAllProducts() {
         return inventoryService.getAllProducts();
     }
